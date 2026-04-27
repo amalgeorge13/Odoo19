327bf3c027c46645920521f80c07a0f8ad3fb01e
amal <amalpulikkottil@gmail.com>
2026-04-27 17:27:21 +0530

    [ADD] Apr27

--- auto_set_task/models/project_task.py
+++ auto_set_task/models/project_task.py
@@ -6,13 +6,24 @@ class ProjectTask(models.Model):
     @api.onchange('tag_ids')
     def _onchange_tag_ids(self):
         # self.user_ids=
+        assignees=[]
         tags=[]
+        if not assignees:
+            self.user_ids = False
         for tag in self.tag_ids:
             tags.append(tag.name)
         print(tags)
-        # users=self.env['res.users'].search([('category_ids','=',True)])
-        # print(users)
+        users=self.env['res.users'].search([('active','=',True)])
+        for user in users:
+            if user.category_ids:
+                for tag in user.category_ids:
+                    if tag.name in tags:
+                        assignees.append(user.id)
+                        print(assignees)
+                        print(tag.name)
+        self.user_ids = assignees
 
-
-
-        # print(self.env['res.users'].search([('category_ids','=',True)]))
+        @api.onchange('personal_stage_id')
+        def onchange_stage_id(self):
+            # if self.personal_stage_id == "01_in_progress":
+                print(12345)
